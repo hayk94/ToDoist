e81fbf79f9640a575d24b8303aa6810c64d05a69
Hayk <hayk94@gmail.com>
2018-03-06 15:09:58 +0400

    clean boilerplate and add container

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Container from './Container'
 
-import ImageWrapper from './ImageWrapper.jsx'
-
-import cat from 'images/cat.jpg'
-import dog from 'images/dog.jpg'
-import dogAndCat from 'images/dogAndCat.jpg'
-
-const App = (props) => {
-  return (
-    <div className='container'>
-      <ImageWrapper src={cat} />
-      <ImageWrapper src={dog} />
-      <ImageWrapper src={dogAndCat} />
-    </div>
-  )
-}
+const App = (props) => <MuiThemeProvider>
+  <Container />
+</MuiThemeProvider>
 
 export default App
---
+++ src/app/Container.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Container extends Component {
+  render () {
+    return (
+      <div>CONTAINER</div>
+    )
+  }
+}
+
+export default Container
